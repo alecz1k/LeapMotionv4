6bd61199d7181d4343173d64de093b8b0c2ad9b5
alecz1k <alecz1k@gmail.com>
2020-10-06 16:31:54 -0700

    Added Grabbable Object
    Added Grabbable Object

--- Assets/Scripts/MyGestureActivation.cs
+++ Assets/Scripts/MyGestureActivation.cs
@@ -35,7 +35,7 @@ public class MyGestureActivation : MonoBehaviour
         vignette.intensity.value = 0;
         chromatic.intensity.value = 0;
         colorGrading.saturation.value = 1;
-        depth.focusDistance.value = 3;
+        depth.focusDistance.value = 5;
 
     }
 
@@ -48,7 +48,7 @@ public class MyGestureActivation : MonoBehaviour
             prefab.SetActive(true);
             bloom.intensity.value = Mathf.Lerp(bloom.intensity.value, 50, .075f * Time.deltaTime);
             colorGrading.saturation.value = Mathf.Lerp(colorGrading.saturation.value, 100, .075f * Time.deltaTime);
-            depth.focusDistance.value = Mathf.Lerp(depth.focusDistance.value, 5, .025f * Time.deltaTime);
+            depth.focusDistance.value = Mathf.Lerp(depth.focusDistance.value, 1, .025f * Time.deltaTime);
             vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, 1, .05f * Time.deltaTime);
             chromatic.intensity.value = Mathf.Lerp(chromatic.intensity.value, 1, .05f * Time.deltaTime);
             Debug.Log("Indexes and Thumbs Are Touching, We are Gesture Ready");
@@ -62,7 +62,7 @@ public class MyGestureActivation : MonoBehaviour
             gestureIsOn = false;
             bloom.intensity.value = Mathf.Lerp(bloom.intensity.value, 1, .025f * Time.deltaTime);
             colorGrading.saturation.value = Mathf.Lerp(colorGrading.saturation.value, -50, .025f * Time.deltaTime);
-            depth.focusDistance.value = Mathf.Lerp(depth.focusDistance.value, 3, .015f * Time.deltaTime);
+            depth.focusDistance.value = Mathf.Lerp(depth.focusDistance.value, 5, .015f * Time.deltaTime);
             vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, 0, .025f * Time.deltaTime);
             chromatic.intensity.value = Mathf.Lerp(chromatic.intensity.value, 0, .025f * Time.deltaTime);
         }
